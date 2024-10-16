7b8635f2135108c70f51dead489a3cb5a8364c89
ginnytruong <ginnytruong98@gmail.com>
2024-10-16 16:02:47 +0100

    fixed create account redirect issue

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setDoc, doc } from "firebase/firestore";
